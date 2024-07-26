bd8966b530623b278b59bc5f9fb900cb00585a5f
daniel.vladco <vladcodaniel@gmail.com>
2024-07-26 15:30:21 +0300

    fix: Hk Hv and Hs

--- internal/block/header.go
+++ internal/block/header.go
@@ -5,16 +5,25 @@ import (
 	"github.com/eigerco/strawberry/internal/time"
 )
 
+const NumberOfValidators uint16 = 1023
+
 // Header as defined in the section 5 in the paper
 type Header struct {
 	ParentHash           crypto.Hash                     // Hp
 	PriorStateRoot       crypto.Hash                     // Hr
 	ExtrinsicHash        crypto.Hash                     // Hx
 	TimeSlotIndex        time.Timeslot                   // Ht
-	EpochMarker          []crypto.Hash                   // He
+	EpochMarker          *EpochMarker                    // He
 	WinningTicketsMarker [time.TimeslotsPerEpoch]*Ticket // Hw
 	JudgementsMarkers    []crypto.Hash                   // Hj
-	BlockAuthorKey       []byte                          // Hk
-	VRFSignature         []byte                          // Hv
-	BlockSealSignature   []byte                          // Hs
+	PublicKeyIndex       uint16                          // Hk
+	VRFSignature         crypto.BandersnatchSignature    // Hv
+	BlockSealSignature   crypto.BandersnatchSignature    // Hs
+}
+
+// EpochMarker consists of epoch randomness and a sequence of
+// Bandersnatch keys defining the Bandersnatch validator keys (kb) beginning in the next epoch.
+type EpochMarker struct {
+	Keys    [NumberOfValidators]crypto.BandersnatchPublicKey
+	Entropy crypto.Hash
 }
