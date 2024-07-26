241ec1d194485f76c23cdedfba6e348055961ac0
Emanuel Pargov <bamzedev@gmail.com>
2024-07-26 12:02:19 +0300

    Add ticket structs (#18)

--- internal/block/block.go
+++ internal/block/block.go
@@ -1,4 +1,12 @@
 package block
 
+// Block represents the main block structure
 type Block struct {
+	Header    *Header
+	Extrinsic *Extrinsic
+}
+
+// Extrinsic represents the block extrinsic data
+type Extrinsic struct {
+	ET []*TicketProof
 }
