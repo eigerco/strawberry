8d64844c0ea44780173e9dc9193b6ec43d2733ef
Emanuel Pargov <bamzedev@gmail.com>
2024-07-30 19:27:05 +0300

    Add dispute structs (#23)
    * Add dispute  structs
    
    * Replace judgmentSignatureSize with Ed25519SignatureSize

--- internal/crypto/constants.go
+++ internal/crypto/constants.go
@@ -5,6 +5,7 @@ const (
 	BandersnatchSize     = 32
 	Ed25519PublicSize    = 32
 	Ed25519PrivateSize   = 64
+	Ed25519SignatureSize = 64
 	BLSSize              = 144
 	BandersnatchRingSize = 144
 	MetadataSize         = 128
