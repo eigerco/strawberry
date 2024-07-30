4d9aecf507c4a7242a71f048eb9c1be34e447318
Aran Wilkinson <aran3001@gmail.com>
2024-07-30 08:00:17 +0100

    fix: incorrect time import

--- internal/block/header.go
+++ internal/block/header.go
@@ -2,23 +2,23 @@ package block
 
 import (
 	"github.com/eigerco/strawberry/internal/crypto"
-	"github.com/eigerco/strawberry/internal/time"
+	"github.com/eigerco/strawberry/internal/jamtime"
 )
 
 const NumberOfValidators uint16 = 1023
 
 // Header as defined in the section 5 in the paper
 type Header struct {
-	ParentHash           crypto.Hash                     // Hp
-	PriorStateRoot       crypto.Hash                     // Hr
-	ExtrinsicHash        crypto.Hash                     // Hx
-	TimeSlotIndex        time.Timeslot                   // Ht
-	EpochMarker          *EpochMarker                    // He
-	WinningTicketsMarker [time.TimeslotsPerEpoch]*Ticket // Hw
-	JudgementsMarkers    []crypto.Hash                   // Hj
-	PublicKeyIndex       uint16                          // Hk
-	VRFSignature         crypto.BandersnatchSignature    // Hv
-	BlockSealSignature   crypto.BandersnatchSignature    // Hs
+	ParentHash           crypto.Hash                        // Hp
+	PriorStateRoot       crypto.Hash                        // Hr
+	ExtrinsicHash        crypto.Hash                        // Hx
+	TimeSlotIndex        jamtime.Timeslot                   // Ht
+	EpochMarker          *EpochMarker                       // He
+	WinningTicketsMarker [jamtime.TimeslotsPerEpoch]*Ticket // Hw
+	JudgementsMarkers    []crypto.Hash                      // Hj
+	PublicKeyIndex       uint16                             // Hk
+	VRFSignature         crypto.BandersnatchSignature       // Hv
+	BlockSealSignature   crypto.BandersnatchSignature       // Hs
 }
 
 // EpochMarker consists of epoch randomness and a sequence of
