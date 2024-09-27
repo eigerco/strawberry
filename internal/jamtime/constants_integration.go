e53ca3a9881f396ad5fcebc81895cabf514500b6
Leonidas <lmaroulis2003@gmail.com>
2024-09-27 08:42:32 +0300

    small fixes

--- internal/jamtime/constants_integration.go
+++ internal/jamtime/constants_integration.go
@@ -3,24 +3,8 @@
 package jamtime
 
 const (
-	// MinEpoch represents the first epoch in the JAM protocol.
-	// It corresponds to the epoch containing the JAM Epoch start time
-	// (12:00pm on January 1, 2024 UTC).
-	MinEpoch Epoch = 0
-
-	// MaxEpoch represents the last possible epoch in the JAM protocol.
-	// It is calculated as the maximum value of Epoch (uint32) divided by
-	// TimeslotsPerEpoch. This ensures that the last epoch can contain
-	// a full complement of timeslots without overflowing.
-	MaxEpoch Epoch = ^Epoch(0) / TimeslotsPerEpoch
-
-	// TimeslotsPerEpoch defines the number of timeslots in each epoch.
-	// In the JAM protocol, each epoch consists of exactly 600 timeslots,
-	// as specified in the JAM Graypaper.
-	TimeslotsPerEpoch = 12
-
-	// EpochDuration defines the total duration of each epoch.
-	// It is calculated by multiplying TimeslotsPerEpoch by TimeslotDuration,
-	// resulting in a duration of 1 hour per epoch.
-	EpochDuration = TimeslotsPerEpoch * TimeslotDuration
+	MinEpoch          Epoch = 0
+	MaxEpoch          Epoch = ^Epoch(0) / TimeslotsPerEpoch
+	TimeslotsPerEpoch       = 12
+	EpochDuration           = TimeslotsPerEpoch * TimeslotDuration
 )
