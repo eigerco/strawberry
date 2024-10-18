3171b548d848ad0cc0af8d0a2017eef58cf05927
Leonidas <lmaroulis2003@gmail.com>
2024-10-18 11:32:39 +0300

    Add lookup host function

--- internal/polkavm/host_call/general_functions.go
+++ internal/polkavm/host_call/general_functions.go
@@ -1,17 +1,23 @@
 package host_call
 
 import (
+	"github.com/eigerco/strawberry/internal/block"
 	"github.com/eigerco/strawberry/internal/polkavm"
+	"github.com/eigerco/strawberry/internal/state"
+	"golang.org/x/crypto/blake2b"
+	"math"
 )
 
-const GasRemainingCost = 10
+const (
+	GasRemainingCost = 10
+	LookupCost       = 10
+)
 
 // GasRemaining ΩG
 func GasRemaining(instance polkavm.Instance) error {
-	if instance.GasRemaining() < GasRemainingCost {
-		return polkavm.ErrOutOfGas
+	if err := deductGas(instance, GasRemainingCost); err != nil {
+		return err
 	}
-	instance.DeductGas(GasRemainingCost)
 
 	ksLower := instance.GetReg(polkavm.A0) // Lower 32 bits
 	ksUpper := instance.GetReg(polkavm.A1) // Upper 32 bits
@@ -24,6 +30,82 @@ func GasRemaining(instance polkavm.Instance) error {
 	// Split the new ξ' value into its lower and upper parts.
 	instance.SetReg(polkavm.A0, uint32(ks&((1<<32)-1)))
 	instance.SetReg(polkavm.A1, uint32(ks>>32))
+
+	return nil
+}
+
+// Lookup ΩL
+func Lookup(instance polkavm.Instance, memoryMap *polkavm.MemoryMap, serviceId block.ServiceId, serviceState state.ServiceState) error {
+	if err := deductGas(instance, LookupCost); err != nil {
+		return err
+	}
+
+	omega0 := instance.GetReg(polkavm.A0)
+
+	// Determine the lookup key 'a'
+	var a state.ServiceAccount
+	if omega0 == uint32(serviceId) || omega0 == math.MaxUint32 {
+		// Lookup service account by serviceId in the serviceState
+		serviceAccount, serviceExists := serviceState[serviceId]
+		if !serviceExists {
+			instance.SetReg(polkavm.A0, uint32(polkavm.HostCallResultNone))
+			return nil
+		}
+
+		a = serviceAccount
+	} else {
+		storedService, exists := serviceState[block.ServiceId(omega0)]
+		if !exists {
+			instance.SetReg(polkavm.A0, uint32(polkavm.HostCallResultNone))
+			return nil
+		}
+		a = storedService
+	}
+
+	ho := instance.GetReg(polkavm.A1)
+
+	// Ensure the memory range is valid for hashing (µho..ho+32)
+	memorySlice, err := instance.GetMemory(memoryMap, ho, 32)
+	if err != nil {
+		instance.SetReg(polkavm.A0, uint32(polkavm.HostCallResultOob))
+		return nil
+	}
+
+	// Compute the hash H(µho..ho+32)
+	hash := blake2b.Sum256(memorySlice)
+
+	// Lookup value in storage (v) using the hash
+	v, exists := a.Storage[hash]
+	if !exists {
+		instance.SetReg(polkavm.A0, uint32(polkavm.HostCallResultNone))
+		return nil
+	}
+
+	bo := instance.GetReg(polkavm.A2)
+	bz := instance.GetReg(polkavm.A3)
+
+	// Write value to memory if within bounds
+	if len(v) > 0 && len(v) <= int(bz) {
+		if err = instance.SetMemory(memoryMap, bo, v); err != nil {
+			instance.SetReg(polkavm.A0, uint32(polkavm.HostCallResultOob))
+			return nil
+		}
+	} else {
+		instance.SetReg(polkavm.A0, uint32(polkavm.HostCallResultOob))
+		return nil
+	}
+
+	instance.SetReg(polkavm.A0, uint32(len(v)))
+
+	return nil
+}
+
+func deductGas(instance polkavm.Instance, gasCost int64) error {
+	if instance.GasRemaining() < gasCost {
+		return polkavm.ErrOutOfGas
+	}
+
+	instance.DeductGas(gasCost)
 
 	return nil
 }
--- internal/polkavm/host_call/general_functions_test.go
+++ internal/polkavm/host_call/general_functions_test.go
@@ -1,11 +1,15 @@
 package host_call_test
 
 import (
+	"github.com/eigerco/strawberry/internal/block"
+	"github.com/eigerco/strawberry/internal/crypto"
 	"github.com/eigerco/strawberry/internal/polkavm"
 	"github.com/eigerco/strawberry/internal/polkavm/host_call"
 	"github.com/eigerco/strawberry/internal/polkavm/interpreter"
+	"github.com/eigerco/strawberry/internal/state"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/blake2b"
 	"testing"
 )
 
@@ -37,4 +41,76 @@ func TestGasRemaining(t *testing.T) {
 
 	assert.Equal(t, expectedGas, instance.GasRemaining())
 	assert.Equal(t, expectedGas, (int64(instance.GetReg(polkavm.A1))<<32)|int64(instance.GetReg(polkavm.A0)))
+}
+
+func TestLookup(t *testing.T) {
+	pp := &polkavm.Program{
+		RODataSize: 0,
+		RWDataSize: 256,
+		StackSize:  512,
+	}
+
+	memoryMap, err := polkavm.NewMemoryMap(
+		polkavm.VmMinPageSize,
+		pp.RODataSize,
+		pp.RWDataSize,
+		pp.StackSize,
+		pp.ROData,
+	)
+	require.NoError(t, err)
+
+	module, err := interpreter.NewModule(pp, memoryMap)
+	require.NoError(t, err)
+
+	initialGas := int64(100)
+
+	// Service Not Found
+	instance := module.Instantiate(0, initialGas)
+	instance.SetReg(polkavm.A0, 1)
+
+	err = host_call.Lookup(instance, memoryMap, 1, make(state.ServiceState))
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(polkavm.HostCallResultNone), instance.GetReg(polkavm.A0))
+	assert.Equal(t, initialGas-host_call.LookupCost, instance.GasRemaining())
+
+	// Successful Key Lookup
+	instance = module.Instantiate(0, 100)
+	serviceId := block.ServiceId(1)
+	val := []byte("value to store")
+
+	ho := memoryMap.RWDataAddress
+	bo := memoryMap.RWDataAddress + 100
+
+	dataToHash := make([]byte, 32)
+	copy(dataToHash, "hash")
+
+	hash := blake2b.Sum256(dataToHash)
+
+	serviceState := state.ServiceState{
+		serviceId: state.ServiceAccount{
+			Storage: map[crypto.Hash][]byte{
+				hash: val,
+			},
+		},
+	}
+
+	err = instance.SetMemory(memoryMap, ho, dataToHash)
+	require.NoError(t, err)
+
+	// Set the registers
+	instance.SetReg(polkavm.A0, uint32(serviceId))
+	instance.SetReg(polkavm.A1, ho)
+	instance.SetReg(polkavm.A2, bo)
+	instance.SetReg(polkavm.A3, 32)
+
+	// Perform the lookup
+	err = host_call.Lookup(instance, memoryMap, serviceId, serviceState)
+	assert.NoError(t, err)
+
+	actualValue, err := instance.GetMemory(memoryMap, bo, len(val))
+	require.NoError(t, err)
+
+	assert.Equal(t, val, actualValue)
+	assert.Equal(t, uint32(len(val)), instance.GetReg(polkavm.A0))
+	assert.Equal(t, initialGas-host_call.LookupCost, instance.GasRemaining())
 }
