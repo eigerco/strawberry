bdf725a89817dfb0ab11cc75c2286eaece050429
carlos-romano <carlos@eiger.co>
2024-10-22 12:33:10 +0200

    Add trie integration test with test vectors and needed refactoring (#80)

--- internal/state/merkle_test.go
+++ internal/state/merkle_test.go
@@ -35,18 +35,3 @@ func TestMerklizeState_DifferentState(t *testing.T) {
 
 	assert.NotEqual(t, rootHash1, rootHash2, "Different states should produce different Merkle roots.")
 }
-
-// TestGetBit tests the getBit utility function.
-func TestGetBit(t *testing.T) {
-	byteSeq := []byte{0xA2} // 10100010 in binary
-
-	// Test individual bits
-	assert.Equal(t, byte(1), getBit(byteSeq, 0), "The first bit should be 1.")
-	assert.Equal(t, byte(0), getBit(byteSeq, 1), "The second bit should be 0.")
-	assert.Equal(t, byte(1), getBit(byteSeq, 2), "The third bit should be 1.")
-	assert.Equal(t, byte(0), getBit(byteSeq, 3), "The fourth bit should be 0.")
-	assert.Equal(t, byte(0), getBit(byteSeq, 4), "The fifth bit should be 0.")
-	assert.Equal(t, byte(0), getBit(byteSeq, 5), "The sixth bit should be 0.")
-	assert.Equal(t, byte(1), getBit(byteSeq, 6), "The seventh bit should be 1.")
-	assert.Equal(t, byte(0), getBit(byteSeq, 7), "The eighth bit should be 0.")
-}
