6fe007db563f72534f88e6800746c74d5605c83c
Emanuel Pargov <bamzedev@gmail.com>
2024-11-18 14:06:24 +0200

    Add keccak256 (#118)

--- internal/crypto/hash.go
+++ internal/crypto/hash.go
@@ -1,10 +1,24 @@
 package crypto
 
-import "golang.org/x/crypto/blake2b"
+import (
+	"golang.org/x/crypto/blake2b"
+	"golang.org/x/crypto/sha3"
+)
 
 type Hash [HashSize]byte
 
 func HashData(data []byte) Hash {
 	hash := blake2b.Sum256(data)
 	return hash
+}
+
+// KeccakData hashes the input data using Keccak-256
+func KeccakData(data []byte) Hash {
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(data)
+	hashed := hash.Sum(nil)
+
+	var result Hash
+	copy(result[:], hashed)
+	return result
 }
