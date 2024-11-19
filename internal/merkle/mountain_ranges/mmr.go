0cfd720df7c80d85f963f1a90904f73dbff2727c
carlos-romano <carlos@eiger.co>
2024-11-19 07:32:58 +0100

    Modify mmr implementation and adapt tests to match specification. (#121)

--- internal/merkle/mountain_ranges/mmr.go
+++ internal/merkle/mountain_ranges/mmr.go
@@ -6,36 +6,36 @@ import (
 	"github.com/eigerco/strawberry/pkg/serialization/codec"
 )
 
-type MMR struct {
-	peaks []*crypto.Hash // nil represents empty peak (Ø)
-}
+type MMR struct{}
 
 func New() *MMR {
-	return &MMR{peaks: []*crypto.Hash{}}
+	return &MMR{}
 }
 
-// Append (A function): A(r, l, H) ↦ P(r, l, 0, H)
-func (m *MMR) Append(item []byte, hashFunc func([]byte) crypto.Hash) error {
-	hash := hashFunc(item)
-	m.peaks = placePeak(m.peaks, &hash, 0, hashFunc)
-	return nil
+// Append (A function): A(r, l, H) ↦ P(r, l, 0, H) from equation (327)
+func (m *MMR) Append(r []*crypto.Hash, l crypto.Hash, hashFunc func([]byte) crypto.Hash) []*crypto.Hash {
+	hash := hashFunc(l[:])
+	return placePeak(r, &hash, 0, hashFunc)
 }
 
 // placePeak implements P function from equation (327):
-// P(r, l, n, H) ↦ { r ++ l                                    if n ≥ |r|
+// P(r, l, n, H) ↦ { r ++ l    if n ≥ |r|
 //
-//	{ R(r, n, l)                               if n < |r| ∧ rₙ = Ø
+//	{ R(r, n, l)               if n < |r| ∧ rₙ = Ø
 //	{ P(R(r, n, Ø), H(rₙ ~ l), n + 1, H)      otherwise
 func placePeak(peaks []*crypto.Hash, item *crypto.Hash, position int, hashFunc func([]byte) crypto.Hash) []*crypto.Hash {
 	if position >= len(peaks) {
+		// r ++ l case
 		return append(peaks, item)
 	}
 
-	if position < len(peaks) && peaks[position] == nil {
+	if peaks[position] == nil {
+		// R(r, n, l) case
 		return replacePeakAt(peaks, position, item)
 	}
 
-	combined := append(peaks[position][:], item[:]...)
+	// P(R(r, n, Ø), H(rₙ ~ l), n + 1, H) case
+	combined := append((*peaks[position])[:], (*item)[:]...)
 	hash := hashFunc(combined)
 	return placePeak(replacePeakAt(peaks, position, nil), &hash, position+1, hashFunc)
 }
@@ -52,18 +52,13 @@ func replacePeakAt(peaks []*crypto.Hash, index int, value *crypto.Hash) []*crypt
 }
 
 // Encode implements E_M function from equation (328):
-func (m *MMR) Encode() ([]byte, error) {
+func (m *MMR) Encode(peaks []*crypto.Hash) ([]byte, error) {
 	jamCodec := codec.NewJamCodec()
 	serializer := serialization.NewSerializer(jamCodec)
-	encoded, err := serializer.Encode(m.peaks)
+
+	encoded, err := serializer.Encode(peaks)
 	if err != nil {
 		return nil, err
 	}
 	return encoded, nil
-}
-
-func (m *MMR) GetPeaks() []*crypto.Hash {
-	result := make([]*crypto.Hash, len(m.peaks))
-	copy(result, m.peaks)
-	return result
 }
