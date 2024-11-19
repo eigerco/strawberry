ab5c2dc97229ce457b37b1fc458142349f059d24
Daniel Vladco <vladcodaniel@gmail.com>
2024-11-19 14:41:12 +0200

    feat: validate guarantee extrinsic (#123)

--- internal/block/header.go
+++ internal/block/header.go
@@ -5,6 +5,9 @@ import (
 	"github.com/eigerco/strawberry/internal/common"
 	"github.com/eigerco/strawberry/internal/crypto"
 	"github.com/eigerco/strawberry/internal/jamtime"
+	"github.com/eigerco/strawberry/pkg/serialization"
+	"github.com/eigerco/strawberry/pkg/serialization/codec"
+	"sync"
 )
 
 // Header as defined in the section 5 in the paper
@@ -26,4 +29,45 @@ type Header struct {
 type EpochMarker struct {
 	Entropy crypto.Hash
 	Keys    [common.NumberOfValidators]crypto.BandersnatchPublicKey
+}
+
+// AncestorStoreSingleton the in memory store for headers that need to be kept for 24 hours
+// TODO replace with pebble
+var AncestorStoreSingleton = &AncestorStore{
+	ancestorSet: make(map[crypto.Hash]*Header),
+	mu:          sync.RWMutex{},
+}
+
+type AncestorStore struct {
+	ancestorSet map[crypto.Hash]*Header
+	mu          sync.RWMutex
+}
+
+func (a *AncestorStore) StoreHeader(header *Header) error {
+	encodedHeader, err := serialization.NewSerializer(codec.NewJamCodec()).Encode(header)
+	if err != nil {
+		return err
+	}
+	hash := crypto.HashData(encodedHeader)
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	a.ancestorSet[hash] = header
+	return nil
+}
+
+func (a *AncestorStore) GetAncestor(header *Header) *Header {
+	a.mu.RLock()
+	defer a.mu.RUnlock()
+	return a.ancestorSet[header.ParentHash]
+}
+
+func (a *AncestorStore) FindAncestor(fn func(header *Header) bool) *Header {
+	a.mu.RLock()
+	defer a.mu.RUnlock()
+	for _, h := range a.ancestorSet {
+		if fn(h) {
+			return h
+		}
+	}
+	return nil
 }
