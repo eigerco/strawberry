0ab271befdfb2c8dcb9e1dc9242985495a926c01
carlos-romano <carlos@eiger.co>
2025-03-12 08:58:58 +0100

    Add preimages test vectors (#283)
    * Add preimages test vectors

---
+++ tests/integration/preimages_test.go
@@ -0,0 +1,203 @@
+//go:build integration
+
+package integration_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/eigerco/strawberry/internal/store"
+	"github.com/eigerco/strawberry/pkg/db/pebble"
+
+	"github.com/eigerco/strawberry/internal/statetransition"
+
+	"github.com/eigerco/strawberry/internal/block"
+	"github.com/eigerco/strawberry/internal/crypto"
+	"github.com/eigerco/strawberry/internal/jamtime"
+	"github.com/eigerco/strawberry/internal/service"
+	"github.com/stretchr/testify/require"
+)
+
+func ReadPreimageJSONFile(filename string) (*PreimageTestVector, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %v", err)
+	}
+
+	var data PreimageTestVector
+	if err := json.Unmarshal(bytes, &data); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %v", err)
+	}
+
+	return &data, nil
+}
+
+// PreimageTestVector represents the JSON structure of a preimage test vector
+type PreimageTestVector struct {
+	Input     InputData     `json:"input"`
+	PreState  PreimageState `json:"pre_state"`
+	Output    OutputData    `json:"output"`
+	PostState PreimageState `json:"post_state"`
+}
+
+// InputData represents the input for the preimage test
+type InputData struct {
+	Preimages []PreimageItem `json:"preimages"`
+	Slot      uint64         `json:"slot"`
+}
+
+// PreimageItem represents a preimage in the input
+type PreimageItem struct {
+	Requester uint32 `json:"requester"`
+	Blob      string `json:"blob"`
+}
+
+// PreimageState represents the state in the test vector
+type PreimageState struct {
+	Accounts []AccountData `json:"accounts"`
+}
+
+// AccountData represents account data in state
+type AccountData struct {
+	ID   uint64       `json:"id"`
+	Data ServiceState `json:"data"`
+}
+
+// ServiceState represents the service state in account data
+type ServiceState struct {
+	Preimages  []PreimageData       `json:"preimages"`
+	LookupMeta []PreimageLookupMeta `json:"lookup_meta"`
+}
+
+// PreimageData represents a preimage in service state
+type PreimageData struct {
+	Hash string `json:"hash"`
+	Blob string `json:"blob"`
+}
+
+// PreimageLookupMeta represents preimage metadata
+type PreimageLookupMeta struct {
+	Key   PreimageMetaKey `json:"key"`
+	Value []uint64        `json:"value"`
+}
+
+// PreimageMetaKey represents the key for preimage metadata
+type PreimageMetaKey struct {
+	Hash   string `json:"hash"`
+	Length uint32 `json:"length"`
+}
+
+// OutputData represents the expected output
+type OutputData struct {
+	Ok  interface{} `json:"ok"`
+	Err string      `json:"err"`
+}
+
+func TestPreimage(t *testing.T) {
+	files, err := os.ReadDir("vectors/preimages")
+	require.NoError(t, err, "failed to read directory")
+
+	db, err := pebble.NewKVStore()
+	require.NoError(t, err)
+
+	chain := store.NewChain(db)
+	defer chain.Close()
+
+	for _, file := range files {
+		if !strings.HasSuffix(file.Name(), ".json") {
+			continue
+		}
+
+		t.Run(file.Name(), func(t *testing.T) {
+
+			filePath := fmt.Sprintf("vectors/preimages/%s", file.Name())
+			data, err := ReadPreimageJSONFile(filePath)
+			require.NoError(t, err, "failed to read JSON file: %s", filePath)
+
+			preServiceState := mapServiceState(t, data.PreState)
+			preimages := mapPreimages(t, data.Input.Preimages)
+
+			newTimeSlot := jamtime.Timeslot(data.Input.Slot)
+			newServiceState, err := statetransition.CalculateIntermediateServiceState(preimages, preServiceState, newTimeSlot)
+			if data.Output.Err != "" {
+				require.Error(t, err)
+				require.EqualError(t, err, strings.ReplaceAll(data.Output.Err, "_", " "))
+				return
+			}
+			require.NoError(t, err)
+
+			expectedPostServiceState := mapServiceState(t, data.PostState)
+			require.Equal(t, expectedPostServiceState, newServiceState, "State after transition does not match expected state")
+		})
+	}
+}
+
+// mapPreState converts the pre-state from the test vector to internal service state
+func mapServiceState(t *testing.T, state PreimageState) service.ServiceState {
+	serviceState := make(service.ServiceState)
+
+	for _, account := range state.Accounts {
+		serviceId := block.ServiceId(account.ID)
+		serviceAccount := service.ServiceAccount{
+			Storage:                make(map[crypto.Hash][]byte),
+			PreimageLookup:         make(map[crypto.Hash][]byte),
+			PreimageMeta:           make(map[service.PreImageMetaKey]service.PreimageHistoricalTimeslots),
+			Balance:                1000, // Default values for fields not in test vector
+			GasLimitForAccumulator: 100,
+			GasLimitOnTransfer:     50,
+		}
+
+		// Map existing preimages
+		for _, preimage := range account.Data.Preimages {
+			hash := crypto.Hash(mustStringToHex(preimage.Hash))
+			blob := mustStringToHex(preimage.Blob)
+			serviceAccount.PreimageLookup[hash] = blob
+		}
+
+		// Map preimage metadata
+		for _, meta := range account.Data.LookupMeta {
+			hash := crypto.Hash(mustStringToHex(meta.Key.Hash))
+			length := service.PreimageLength(meta.Key.Length)
+
+			// Convert timeslot array
+			var timeslots service.PreimageHistoricalTimeslots
+			for _, slot := range meta.Value {
+				timeslots = append(timeslots, jamtime.Timeslot(slot))
+			}
+
+			serviceAccount.PreimageMeta[service.PreImageMetaKey{
+				Hash:   hash,
+				Length: length,
+			}] = timeslots
+		}
+
+		serviceState[serviceId] = serviceAccount
+	}
+
+	return serviceState
+}
+
+// mapPreimages converts preimage items from the test vector to internal preimage type
+func mapPreimages(t *testing.T, items []PreimageItem) []block.Preimage {
+	preimages := make([]block.Preimage, len(items))
+	for i, item := range items {
+		blobBytes, err := crypto.StringToHex(item.Blob)
+		require.NoError(t, err, "Failed to parse blob hex")
+
+		preimages[i] = block.Preimage{
+			ServiceIndex: item.Requester,
+			Data:         blobBytes,
+		}
+	}
+	return preimages
+}
