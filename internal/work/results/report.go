bab0b65c233315c861a58c0acbf9ee6db667528c
Leonidas Maroulis <pantrif@users.noreply.github.com>
2025-04-14 20:58:16 +0300

    Work package sharing handler CE-134 (#296)

---
+++ internal/work/results/report.go
@@ -0,0 +1,67 @@
+package results
+
+import (
+	"fmt"
+
+	"github.com/eigerco/strawberry/internal/block"
+	"github.com/eigerco/strawberry/internal/crypto"
+	"github.com/eigerco/strawberry/internal/refine"
+	"github.com/eigerco/strawberry/internal/service"
+	"github.com/eigerco/strawberry/internal/work"
+)
+
+// ProduceWorkReport runs the refine invocation and returns the work report
+func ProduceWorkReport(
+	refine refine.RefinePVMInvoker,
+	serviceState service.ServiceState,
+	authOutput []byte,
+	coreIndex uint16,
+	bundle work.PackageBundle,
+	segmentRootLookup map[crypto.Hash]crypto.Hash,
+) (block.WorkReport, error) {
+	var allWorkResults []block.WorkResult
+	exportOffset := uint64(0)
+
+	// TODO: pass the correct segment data (placeholder for now)
+	var segments []work.Segment
+
+	for index, item := range bundle.Package.WorkItems {
+		refineOut, _, refineErr := refine.InvokePVM(
+			uint32(index),
+			bundle.Package,
+			authOutput,
+			segments,
+			exportOffset,
+		)
+
+		var out block.WorkResultOutputOrError
+
+		if refineErr != nil {
+			out = block.WorkResultOutputOrError{Inner: block.UnexpectedTermination}
+		} else {
+			out = block.WorkResultOutputOrError{Inner: refineOut}
+		}
+
+		wr := item.ToWorkResult(out)
+		allWorkResults = append(allWorkResults, wr)
+		exportOffset += uint64(item.ExportedSegments)
+	}
+
+	_, authHash, err := bundle.Package.ComputeAuthorizerHashes(serviceState)
+	if err != nil {
+		return block.WorkReport{}, fmt.Errorf("failed to compute authorizer hash: %w", err)
+	}
+
+	// (s, x : px, c, a : pa, o, l, r)
+	workReport := block.WorkReport{
+		// TODO: WorkPackageSpecification (14.16) to be constructed as part of the availability logic
+		RefinementContext: bundle.Package.Context,
+		CoreIndex:         coreIndex,
+		Output:            authOutput,
+		AuthorizerHash:    authHash,
+		SegmentRootLookup: segmentRootLookup,
+		WorkResults:       allWorkResults,
+	}
+
+	return workReport, nil
+}
