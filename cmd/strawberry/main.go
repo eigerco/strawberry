d780f5b2fd8e1f2d2d04bcdaa618ad9ed4ed628a
Aki Häkkilä <aki@eiger.co>
2025-07-23 11:29:41 +0300

    minor logging fixes

--- cmd/strawberry/main.go
+++ cmd/strawberry/main.go
@@ -6,9 +6,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
-	"flag"
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"strconv"
@@ -17,6 +15,7 @@ import (
 	"github.com/eigerco/strawberry/internal/safrole"
 	chainState "github.com/eigerco/strawberry/internal/state"
 	"github.com/eigerco/strawberry/internal/validator"
+	"github.com/eigerco/strawberry/pkg/log"
 	"github.com/eigerco/strawberry/pkg/network/node"
 )
 
@@ -26,6 +25,11 @@ type FullValidatorInfo struct {
 	Port       int    `json:"port"`
 	Ed25519Pub string `json:"ed25519_public_key"`
 	Ed25519Prv string `json:"ed25519_private_key"`
+}
+
+type AppConfig struct {
+	LogLevel       string `json:"loglevel"`
+	ValidatorIndex int    `json:"validatorIndex"`
 }
 
 func (f FullValidatorInfo) ToJson() ([]byte, error) {
@@ -38,6 +42,21 @@ func (f FullValidatorInfo) ToJson() ([]byte, error) {
 	f.Ed25519Prv = prvStr
 	f.Ed25519Pub = pubStr
 	return json.MarshalIndent(f, "", "	")
+}
+
+func loadConfig(filename string) (*AppConfig, error) {
+	appConfig := AppConfig{}
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("config file open failed: %v", err)
+	}
+
+	if err := json.NewDecoder(f).Decode(&appConfig); err != nil {
+		return nil, fmt.Errorf("umarshalling application config failed: %v", err)
+	}
+
+	return &appConfig, nil
 }
 
 func loadFullValidatorInfos(filename string) ([]FullValidatorInfo, error) {
@@ -87,54 +106,92 @@ func (f FullValidatorInfo) ToMetadata() ([]byte, error) {
 // go run main.go -index 0
 func main() {
 	ctx := context.Background()
-	index := flag.String("index", "", "Validator Index")
-	flag.Parse()
 
-	if *index == "" {
-		log.Fatal("listen address is required")
+	appConfig, err := loadConfig("appconfig.json")
+	if err != nil {
+		panic("application config load failed:" + err.Error())
 	}
+
+	loglevel, err := log.ParseLogLevel(appConfig.LogLevel)
+	if err != nil {
+		panic("log level parsing failed: " + err.Error())
+	}
+
+	opts := log.Options{LogLevel: loglevel}
+	log.Init(opts)
+
+	maxuint16 := int(^uint16(0))
+	if appConfig.ValidatorIndex < 0 && appConfig.ValidatorIndex > maxuint16 {
+		log.Internal.Fatal().
+			Msgf("validator index %d out of bounds 0-%d", appConfig.ValidatorIndex, maxuint16)
+	}
+
+	index := uint16(appConfig.ValidatorIndex)
+
 	vs, err := loadFullValidatorInfos("test_validators.json")
 	if err != nil {
-		log.Fatal(err)
+		log.Internal.Fatal().
+			Err(err).
+			Msg("loading validator configuration failed")
 	}
-	i, err := strconv.Atoi(*index)
-	if err != nil {
-		log.Fatal(err)
+
+	if int(index) >= len(vs) {
+		log.Internal.Fatal().
+			Uint16("index", index).
+			Msg("validator configuration index out of bounds")
 	}
-	if i < 0 || i >= len(vs) {
-		log.Fatalf("index %d out of bounds", i)
-	}
-	address := vs[i].IP
-	port := vs[i].Port
+	address := vs[index].IP
+	port := vs[index].Port
 	udpAddress, err := net.ResolveUDPAddr("udp", net.JoinHostPort(address, strconv.Itoa(port)))
 	if err != nil {
-		log.Fatal(err)
+		log.Internal.Fatal().
+			Str("address", address).
+			Int("port", port).
+			Err(err).
+			Msg("address resolve failed")
 	}
-	fmt.Printf("listening on: %v\n", address)
-	prv, err := hex.DecodeString(vs[i].Ed25519Prv)
+
+	log.Internal.Info().
+		Msgf("listening on: %v", address)
+
+	prv, err := hex.DecodeString(vs[index].Ed25519Prv)
 	if err != nil {
-		log.Fatal(err)
+		log.Internal.Fatal().
+			Err(err).
+			Msg("own private key decode failed")
 	}
-	pub, err := hex.DecodeString(vs[i].Ed25519Pub)
+	pub, err := hex.DecodeString(vs[index].Ed25519Pub)
 	if err != nil {
-		log.Fatal(err)
+		log.Internal.Fatal().
+			Err(err).
+			Msg("own public key decode failed")
 	}
+
 	privateKey := ed25519.PrivateKey(prv)
 	publicKey := ed25519.PublicKey(pub)
-	vk := validator.ValidatorKeys{
+	vkeys := validator.ValidatorKeys{
 		EdPrv: privateKey,
 		EdPub: publicKey,
 	}
 	validatorsData := safrole.ValidatorsData{}
+
 	for i, k := range vs {
 		pub, err := hex.DecodeString(k.Ed25519Pub)
 		if err != nil {
-			log.Fatal(err)
+			log.Internal.Fatal().
+				Int("index", i).
+				Err(err).
+				Msg("validator public key decode failed")
 		}
+
 		meta, err := k.ToMetadata()
 		if err != nil {
-			log.Fatal(err)
+			log.Internal.Fatal().
+				Int("index", i).
+				Err(err).
+				Msg("validator metadata decode failed")
 		}
+
 		vk := crypto.ValidatorKey{
 			Ed25519:  ed25519.PublicKey(pub),
 			Metadata: crypto.MetadataKey(meta),
@@ -152,13 +209,17 @@ func main() {
 		ValidatorState: vstate,
 	}
 
-	node, err := node.NewNode(ctx, udpAddress, vk, state, uint16(i))
+	node, err := node.NewNode(ctx, udpAddress, vkeys, state, index)
 	if err != nil {
-		log.Fatal(err)
+		log.Internal.Fatal().
+			Err(err).
+			Msg("node creation failed")
 	}
 	err = node.Start()
 	if err != nil {
-		log.Fatal(err)
+		log.Internal.Fatal().
+			Err(err).
+			Msg("node start failed")
 	}
 	select {}
 }
