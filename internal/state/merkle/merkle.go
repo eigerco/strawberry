01e0b8901ba90381b6179e4f294333244e68655c
Oliver Powell <oliver@opowell.com>
2025-12-19 15:35:32 +0100

    Merklize state only in header verification (#505)

--- internal/state/merkle/merkle.go
+++ internal/state/merkle/merkle.go
@@ -2,6 +2,7 @@ package merkle
 
 import (
 	"github.com/eigerco/strawberry/internal/crypto"
+	"github.com/eigerco/strawberry/internal/merkle/trie"
 	"github.com/eigerco/strawberry/internal/state"
 	"github.com/eigerco/strawberry/internal/state/serialization"
 	"github.com/eigerco/strawberry/internal/store"
@@ -27,4 +28,27 @@ func MerklizeState(s state.State, store *store.Trie) (crypto.Hash, error) {
 		return crypto.Hash{}, err
 	}
 	return rootHash, nil
+}
+
+func MerklizeStateOnly(s state.State) (crypto.Hash, error) {
+	// Serialize the state
+	serializedState, err := serialization.SerializeState(s)
+	if err != nil {
+		return crypto.Hash{}, err
+	}
+
+	// Convert the serialized state map to the key-value format that Merklize expects
+	var kvs [][2][]byte
+	for key, value := range serializedState {
+		kvs = append(kvs, [2][]byte{key[:], value})
+	}
+
+	root, err := trie.Merklize(kvs, 0,
+		func(hash crypto.Hash, node trie.Node) error { return nil },
+		func(value []byte) error { return nil })
+	if err != nil {
+		return crypto.Hash{}, err
+	}
+
+	return root, nil
 }
